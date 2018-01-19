ff34e997517d961c55035cb0e45b7e94c56ca14a
qianlongxu <qianlongxu@sohu-inc.com>
2018-01-19 14:04:55 +0800

    整理代码，抽取 MRAudioPlayer 。

--- FFmpeg005/FFmpeg005/Player/MRAudioPlayer.h
+++ FFmpeg005/FFmpeg005/Player/MRAudioPlayer.h
@@ -1,13 +1,15 @@
 //
-//  ViewController.h
+//  MRAudioPlayer.h
 //  FFmpeg005
 //
 //  Created by 许乾隆 on 2018/1/19.
 //  Copyright © 2018年 Awesome FFmpeg Study Demo. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
+@interface MRAudioPlayer : NSObject
+
+- (void)playURLString:(NSString *)url;
 
 @end
