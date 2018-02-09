c44c6c6167b714fcc64d6a57121efe729ca96daf
qianlongxu <qianglongxu@home.mbp>
2018-02-10 00:30:52 +0800

    update date.

--- FFmpeg007/FFmpeg007/main.m
+++ FFmpeg007/FFmpeg007/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  FFmpeg002
 //
-//  Created by Matt Reach on 2018/1/20.
+//  Created by Matt Reach on 2018/2/10.
 //  Copyright © 2017年 Awesome FFmpeg Study Demo. All rights reserved.
 //  开源地址: https://github.com/debugly/StudyFFmpeg
 //
