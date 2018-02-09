38f7560cfe3b5b24835fa6de858ed9f10cd9ad9d
qianlongxu <qianglongxu@home.mbp>
2018-02-09 23:51:01 +0800

    setup 006.

---
+++ FFmpeg006/FFmpeg006/VideoPlayer/MRVideoFrame.m
@@ -0,0 +1,13 @@
+//
+//  MRVideoFrame.m
+//  FFmpeg004
+//
+//  Created by Matt Reach on 2018/12/18.
+//  Copyright © 2018年 Awesome FFmpeg Study Demo. All rights reserved.
+//
+
+#import "MRVideoFrame.h"
+
+@implementation MRVideoFrame
+
+@end
