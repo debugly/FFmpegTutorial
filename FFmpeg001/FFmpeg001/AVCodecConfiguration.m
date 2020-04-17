1b9e0ff92a9481dc068ed2d94440d069faf9031f
qianlongxu <summerhanada@163.com>
2020-04-17 22:09:39 +0800

    share schemes

--- FFmpeg001/FFmpeg001/AVCodecConfiguration.m
+++ FFmpeg001/FFmpeg001/AVCodecConfiguration.m
@@ -19,7 +19,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    ///读取到编译时的配置信息，说名库是正常的，可以用了！
+    ///读取到编译时的配置信息，验证库是否正常的！
     NSString *codec_config = [NSString stringWithCString:avcodec_configuration() encoding:NSUTF8StringEncoding];
     
     int version = avcodec_version();
