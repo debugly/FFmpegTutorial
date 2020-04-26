3c5c5b417c1a5bca6f0018b544b0ced7ce0138a5
qianlongxu <summerhanada@163.com>
2020-04-26 22:16:27 +0800

    add 0x02 demo.

---
+++ Example/iOS/FFmpegTutorial-iOS/0x02/MR0x02ViewController.h
@@ -0,0 +1,17 @@
+//
+//  MR0x02ViewController.h
+//  FFmpegTutorial-iOS
+//
+//  Created by qianlongxu on 2020/4/25.
+//  Copyright © 2020 Matt Reach's Awesome FFmpeg Tutotial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MR0x02ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/iOS/FFmpegTutorial-iOS/0x02/MR0x02ViewController.m
@@ -0,0 +1,32 @@
+//
+//  MR0x02ViewController.m
+//  FFmpegTutorial-iOS
+//
+//  Created by qianlongxu on 2020/4/25.
+//  Copyright © 2020 Matt Reach's Awesome FFmpeg Tutotial. All rights reserved.
+//
+
+#import "MR0x02ViewController.h"
+
+@interface MR0x02ViewController ()
+
+@end
+
+@implementation MR0x02ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
