ee58954d717a0027a343690c02ff4288e293e8c7
qianlongxu <summerhanada@163.com>
2021-07-08 00:20:14 +0800

    【iOS】use https scheme

--- Example/iOS/FFmpegTutorial-iOS/0x02/MR0x02ViewController.m
+++ Example/iOS/FFmpegTutorial-iOS/0x02/MR0x02ViewController.m
@@ -32,7 +32,7 @@
     // Do any additional setup after loading the view.
     [self.indicatorView startAnimating];
     FFPlayer0x02 *player = [[FFPlayer0x02 alloc] init];
-    player.contentPath = @"http://data.vod.itc.cn/?new=/73/15/oFed4wzSTZe8HPqHZ8aF7J.mp4&vid=77972299&plat=14&mkey=XhSpuZUl_JtNVIuSKCB05MuFBiqUP7rB&ch=null&user=api&qd=8001&cv=3.13&uid=F45C89AE5BC3&ca=2&pg=5&pt=1&prod=ifox";
+    player.contentPath = KTestVideoURL1;
     [player prepareToPlay];
     __weakSelf__
     [player openStream:^(NSError * _Nullable error, NSString * _Nullable info) {
--- Example/iOS/FFmpegTutorial-iOS/0x04/MR0x04ViewController.m
+++ Example/iOS/FFmpegTutorial-iOS/0x04/MR0x04ViewController.m
@@ -43,7 +43,7 @@
     self.textView.layoutManager.allowsNonContiguousLayout = NO;
     
     FFPlayer0x04 *player = [[FFPlayer0x04 alloc] init];
-    player.contentPath = @"http://data.vod.itc.cn/?new=/73/15/oFed4wzSTZe8HPqHZ8aF7J.mp4&vid=77972299&plat=14&mkey=XhSpuZUl_JtNVIuSKCB05MuFBiqUP7rB&ch=null&user=api&qd=8001&cv=3.13&uid=F45C89AE5BC3&ca=2&pg=5&pt=1&prod=ifox";
+    player.contentPath = KTestVideoURL1;
     
     __weakSelf__
     [player onError:^{
