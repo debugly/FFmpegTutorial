0aa42ba56086b97ade45d4f1f8288515504889cd
qianlongxu <summerhanada@163.com>
2021-07-26 22:07:06 +0800

    【macOS】MRVideoRendererProtocol

--- Example/macOS/FFmpegTutorial-macOS/0x13/MR0x13VideoRenderer.h
+++ Example/macOS/FFmpegTutorial-macOS/0x13/MR0x13VideoRenderer.h
@@ -8,19 +8,12 @@
 
 #import <Cocoa/Cocoa.h>
 #import <CoreMedia/CMSampleBuffer.h>
+#import "MRVideoRendererProtocol.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef enum : NSUInteger {
-    MRViewContentModeScaleToFill,
-    MRViewContentModeScaleAspectFill,
-    MRViewContentModeScaleAspectFit
-} MRViewContentMode;
+@interface MR0x13VideoRenderer : NSView<MRVideoRendererProtocol>
 
-@interface MR0x13VideoRenderer : NSView
-
-- (void)setContentMode:(MRViewContentMode)contentMode;
-- (MRViewContentMode)contentMode;
 - (void)enqueueSampleBuffer:(CMSampleBufferRef)buffer;
 - (void)cleanScreen;
 
