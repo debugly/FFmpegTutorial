cddb9c1cd083ef3d01d2d1b8f066f5d6cee0820c
qianlongxu <qianlongxu@gmail.com>
2022-01-21 18:51:28 +0800

    【macOS】prepare use fbo tutorial.

--- Example/macOS/FFmpegTutorial-macOS/Root/RootViewController.m
+++ Example/macOS/FFmpegTutorial-macOS/Root/RootViewController.m
@@ -167,6 +167,11 @@
             @"class":@"MR0x155ViewController",
         },
         @{
+            @"title":@"0x15-6",
+            @"detail":@"使用 OpenGL 3.3 FBO 渲染 NV21 视频桢",
+            @"class":@"MR0x156ViewController",
+        },
+        @{
             @"title":@"0x20",
             @"detail":@"使用 AudioUnit 渲染音频桢",
             @"class":@"MR0x20ViewController",
