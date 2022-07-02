8f713102c1324c185ae2fab026978178ad4d3dbd
qianlongxu <summerhanada@163.com>
2022-07-02 23:41:49 +0800

    【macOS】0x141、0x142、0x143、0x144、0x145 code restructure.

--- Example/macOS/FFmpegTutorial-macOS/0x14-2/MR0x142ViewController.h
+++ Example/macOS/FFmpegTutorial-macOS/0x14-2/MR0x142ViewController.h
@@ -5,9 +5,17 @@
 //  Created by qianlongxu on 2021/8/1.
 //  Copyright © 2021 Matt Reach's Awesome FFmpeg Tutotial. All rights reserved.
 //
-// 使用 OpenGL 渲染视频桢
-// 封装 OpenGL 相关逻辑，抽取 MROpenGLCompiler
-// 渲染 YUV420P
+// 0x03:查看音视频流信息
+// 0x04:创建读包线程，读取音视频包
+// 0x05:音视频解码
+// 0x06:创建解码器类
+// 0x10:创建视频缩放类
+// 0x11:使用 Core Graphics 渲染视频桢
+// 0x12:使用 Core Animation 渲染视频桢
+// 0x13:使用 Core Media 渲染视频桢
+// 0x14:使用 OpenGL 渲染 NV12 视频桢
+// 0x141:抽取 OpenGLCompiler 类，封装 OpenGL Shader 相关逻辑
+// 0x142:使用 OpenGL 渲染 YUV420P 视频桢
 
 #import <Cocoa/Cocoa.h>
 
