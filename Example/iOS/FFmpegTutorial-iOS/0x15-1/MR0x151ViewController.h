0b9ee78cac755b33f37b459f80acb0ea278b303a
qianlongxu <summerhanada@163.com>
2022-09-30 23:17:46 +0800

    【iOS】init 0x151 section.

---
+++ Example/iOS/FFmpegTutorial-iOS/0x15-1/MR0x151ViewController.h
@@ -0,0 +1,18 @@
+//
+//  MR0x151ViewController.h
+//  FFmpegTutorial-iOS
+//
+//  Created by qianlongxu on 2020/7/10.
+//  Copyright © 2020 Matt Reach's Awesome FFmpeg Tutotial. All rights reserved.
+//
+// 使用 OpenGL ES 渲染视频桢
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MR0x151ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
