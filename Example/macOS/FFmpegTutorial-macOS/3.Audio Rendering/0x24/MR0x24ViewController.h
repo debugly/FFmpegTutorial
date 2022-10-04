e436c59ea6a024557fd851bb2a76903222f7b3eb
qianlongxu <summerhanada@163.com>
2022-10-04 19:52:19 +0800

    【macOS】restructure section name again.

--- Example/macOS/FFmpegTutorial-macOS/3.Audio Rendering/0x24/MR0x24ViewController.h
+++ Example/macOS/FFmpegTutorial-macOS/3.Audio Rendering/0x24/MR0x24ViewController.h
@@ -5,26 +5,6 @@
 //  Created by qianlongxu on 2022/7/14.
 //  Copyright © 2022 Matt Reach's Awesome FFmpeg Tutotial. All rights reserved.
 //
-// 0x03:查看音视频流信息
-// 0x04:创建读包线程，读取音视频包
-// 0x05:音视频解码
-// 0x06:创建解码器类
-// 0x10:创建视频缩放类
-// 0x11:使用 Core Graphics 渲染视频桢
-// 0x12:使用 Core Animation 渲染视频桢
-// 0x13:使用 Core Media 渲染视频桢
-// 0x14:使用 OpenGL 渲染 NV12 视频桢
-// 0x14-1:抽取 OpenGLCompiler 类，封装 OpenGL Shader 相关逻辑
-// 0x142:使用 OpenGL 渲染 YUV420P 视频桢
-// 0x143:使用 OpenGL 渲染 UYVY422 视频桢
-// 0x144:使用 OpenGL 渲染 YUYV422 视频桢
-// 0x145:使用 OpenGL 渲染 NV21 视频桢
-// 0x151:使用 OpenGL 3 渲染 NV12 视频桢
-// 0x152:使用 OpenGL 3 渲染 YUV420P 视频桢
-// 0x153:使用 OpenGL 3 渲染 UYVY422 视频桢
-// 0x154:使用 OpenGL 3 渲染 YUYV422 视频桢
-// 0x155:使用 OpenGL 3 渲染 NV21 视频桢
-// 0x16:使用 OpenGL 3 FBO 截图
 // 0x20:封装音频重采样类，方便转出指定的采样格式
 // 0x21:使用 AudioUnit 渲染音频桢，解码速度慢，渲染速度快，因此声音断断续续的
 // 0x22:增加Frame缓存队列，解决断断续续问题
